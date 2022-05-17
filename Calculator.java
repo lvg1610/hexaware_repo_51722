a3747baaefdc1544c05301af100ec0c62001d598
lvg1610 <lvg1610@gmail.com>
2022-05-17 12:24:26 +0530

    adding Calc.java First.java 1.0v

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator
+{
+     public static int sum(int x,int y)
+     {
+        return x+y;
+     }
+     public static int square(int x)
+     {
+         return x*x;
+     }
+}  
---
+++ First.java
@@ -0,0 +1,5 @@
+public interface First
+{
+    int sum(int x,int y);
+    int square(int x);
+} 
