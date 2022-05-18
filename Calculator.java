9cf607d2a4ea0a889b74ff15cb198274928569b8
lvg1610 <lvg1610@gmail.com>
2022-05-18 10:22:36 +0530

    Calc and first files added as v2

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator implements First
+{
+    public int sum(int x,int y)
+    {
+        return x+y;
+    }
+    public int square(int x)
+    {
+        return x*x;
+    }
+}
+ 
