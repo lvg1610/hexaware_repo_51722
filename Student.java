1abd8ecf971f6631ec889e82c9cc94120147ea82
lvg1610 <64771079+lvg1610@users.noreply.github.com>
2022-05-18 10:26:02 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student
+{
+      int rnollNo;
+      String name;
+      String[] subjects;
+      public Student()
+      {
+      }
+      public Student(int rollNo,String name,String[] subjects)
+      {
+          this.rollNo = rollNo;
+          this.name = name;
+          this.subjects = subjects; 
+      }
+}
